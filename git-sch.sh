b05469894255acf6c30996adfdd18601ef9a1972
tteokbokki-hater <pires11233@wooooongs.localdomain>
2021-03-10 19:24:58 +0900

    ...

---
+++ git-sch.sh
@@ -0,0 +1,28 @@
+# git-sch.sh
+ 
+Y=$(date +%Y)
+M=$(date +%m)
+D=$(date +%d)
+ 
+Ym=$Y-$M
+Ymd=$Y-$M-$D
+GitRep="git-diary"
+ 
+HomeDir="/home/ubuntu"
+GitDir="$HomeDir/$GitRep"
+FileDir="$HomeDir/$GitRep/$Ym"
+FileNm="$Ymd".md
+ 
+mkdir -p $FileDir
+ 
+echo "### $Ymd 일기" >> $FileDir/$FileNm
+ 
+cd $GitDir
+git add
+git commit -m "commit $FileNm"
+git push origin master
+
+# 권한
+
+$ chmod +x git-sch.sh
+
